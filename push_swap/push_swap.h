8ba20199f3f1fa9c3f7039fbdda06aef6278bea2
Jacob Betting <jabettin@f1r4s3.codam.nl>
2026-03-02 13:51:23 +0100

    stack_init

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jabettin <jabettin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 12:32:12 by jabettin          #+#    #+#             */
-/*   Updated: 2026/03/02 13:19:43 by jabettin         ###   ########.fr       */
+/*   Updated: 2026/03/02 13:46:04 by jabettin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ typedef struct s_node
 }	t_node;
 
 t_node	*create_node(int value);
-
+void	add_back(t_node **stack, t_node *new_node);
+void	free_stack(t_node **stack);
+int		stack_size(t_node *stack);
 #endif
--- push_swap/stack/stack_init.c
+++ push_swap/stack/stack_init.c
@@ -6,7 +6,7 @@
 /*   By: jabettin <jabettin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 12:36:10 by jabettin          #+#    #+#             */
-/*   Updated: 2026/03/02 13:21:06 by jabettin         ###   ########.fr       */
+/*   Updated: 2026/03/02 13:46:58 by jabettin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,45 @@ t_node	*create_node(int value)
 	return (new_node);
 }
 
+void	add_back(t_node **stack, t_node *new_node)
+{
+	t_node	*current;
+
+	if (!stack || !new_node)
+		return ;
+	if (*stack == NULL)
+	{
+		*stack = new_node;
+		return ;
+	}
+	current = *stack;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new_node;
+}
+
+void	free_stack(t_node **stack)
+{
+	t_node	*temp;
+
+	if (!stack || !*stack)
+		return ;
+	while (*stack)
+	{
+		temp = (*stack)->next;
+		free(*stack);
+		*stack = temp;
+	}
+}
+
+int	stack_size(t_node *stack)
+{
+	int	count;
+
+	count = 0;
+	while (stack)
+	{
+		count++;
+		stack = stack->next;
+	}
+}
