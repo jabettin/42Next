90e8e0a749159d44e355d9aae20e9f0d67efb53a
Jacob Betting <jacobahbetting@gmail.com>
2026-03-03 14:18:27 +0100

    added protoype

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jbetting <jbetting@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 12:32:12 by jabettin          #+#    #+#             */
-/*   Updated: 2026/03/03 11:15:58 by jbetting         ###   ########.fr       */
+/*   Updated: 2026/03/03 14:12:24 by jbetting         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,5 @@ t_node	*create_node(int value);
 void	add_back(t_node **stack, t_node *new_node);
 void	free_stack(t_node **stack);
 int		stack_size(t_node *stack);
+void	sa(t_node **stack);
 #endif
