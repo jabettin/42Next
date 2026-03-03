032ddfb36b8fc51d8e78fde15edcb5386a2bc1c5
Jacob Betting <jacobahbetting@gmail.com>
2026-03-03 10:52:14 +0100

    testing adding nodes to the back

---
+++ push_swap/push_swap.c
@@ -0,0 +1,14 @@
+#include "push_swap.h"
+#include <stdio.h>
+
+int main(void)
+{
+	t_node	*a = NULL;
+	add_back(&a, create_node(3));
+	add_back(&a, create_node(7));
+	add_back(&a, create_node(1));
+
+	printf("size: %d\n", stack_size(a));
+	free_stack(&a);
+	return 0;
+}
