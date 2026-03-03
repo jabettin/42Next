21729e065493afef4a03df87146b9a560e0f7d9e
Jacob Betting <jacobahbetting@gmail.com>
2026-03-03 16:25:06 +0100

    changes to atoi

--- push_swap/parsing/atoi_strict.c
+++ push_swap/parsing/atoi_strict.c
@@ -17,17 +17,15 @@ int	atoi_strict(const char *str, long *result)
 			sign =-1;
 		i++;
 	}
-	while (str[i] >= 0 && str[i] <= 9)
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if (!str[i] >= 0 && !str[i] <= 9)
+		*result = *result + (str[i] -'0');
+		if (*result * 10 >= INT_MAX)
 			return 0;
-		*result = *result * 10 + (str[i] -'0');
-		if (sign == 1)
-			if (*result / 10 >= INT_MAX)
-				return 0;
-		if (sign == -1)
-			if (*result / 10 <= INT_MIN)
-				return 0;
+		
+		if (*result / 10 <= INT_MIN)
+			return 0;
+		i++;
 	}
 	
 }
