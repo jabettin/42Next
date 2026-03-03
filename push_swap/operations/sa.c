06b270710935bd00a1727ba1cce3ad86a0138cbd
Jacob Betting <jacobahbetting@gmail.com>
2026-03-03 14:17:29 +0100

    added swap A

---
+++ push_swap/operations/sa.c
@@ -0,0 +1,16 @@
+#include "../push_swap.h"
+
+void	sa(t_node **stack)
+{
+	t_node	*first;
+	t_node	*second;
+
+	if (!stack || !*stack || !(*stack)->next)
+		return;
+	first = *stack;
+	second = first->next;
+
+	first->next = second->next;
+	second->next = first;
+	*stack = second;
+}
