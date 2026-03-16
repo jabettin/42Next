64fb5d750409cf9c587f7426e04fd377452edbf7
Jacob Betting <jacobahbetting@gmail.com>
2026-03-16 16:26:55 +0100

    pb added

--- push_swap/operations/pb.c
+++ push_swap/operations/pb.c
@@ -2,5 +2,15 @@
 
 void	pb(t_node **a, t_node **b)
 {
+	t_node	*temp;
+
+	if (!*a)
+		return ;
+	temp = *a;
+	(*a) = (*a)->next;
+	temp->next = *b;
+	*b = temp;
+	write(1, "pb", 2);	
+	write(1, "\n", 1);
 	
 }
