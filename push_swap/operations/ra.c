b9ad8d3b7a10f5f375d7903b38d544bf307e9541
Jacob Betting <jacobahbetting@gmail.com>
2026-03-17 16:00:48 +0100

    rotate A

---
+++ push_swap/operations/ra.c
@@ -0,0 +1,19 @@
+#include "../push_swap.h"
+
+void	ra(t_node **a)
+{
+	t_node	*temp;
+	t_node	*last;
+	
+	if (!*a || !(*a)->next)
+		return ;
+	temp = *a;
+	*a = (*a)->next;
+	last = *a;
+	while(last->next)
+		last = last->next;
+	last->next = temp;
+	temp->next = NULL;
+	write(1, "ra", 2);
+	write(1, "\n", 1);
+}
