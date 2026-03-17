d7053a63aacd1f32957b06480618620da1b00f5a
Jacob Betting <jacobahbetting@gmail.com>
2026-03-17 20:14:16 +0100

    rra added

---
+++ push_swap/operations/rra.c
@@ -0,0 +1,21 @@
+#include "../push_swap.h"
+
+void	rra(t_node **a)
+{
+	t_node	*last;
+	t_node	*secondlast;
+
+	if(!*a || !(*a)->next)
+		return ;
+		secondlast = *a;
+	while (secondlast->next->next != NULL)
+		secondlast = secondlast->next;
+	last = secondlast->next;
+	secondlast->next = NULL;
+	last->next = *a;
+	*a = last;
+	write(1, "rra", 3);
+	write(1, "\n", 1);
+	
+
+}
