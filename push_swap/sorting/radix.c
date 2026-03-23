3426c4b5a8dc67d34cbc3c00bcf4cad658f9f72a
Jacob Betting <jacobahbetting@gmail.com>
2026-03-23 13:03:18 +0100

    radix added, hopefully done

--- push_swap/sorting/radix.c
+++ push_swap/sorting/radix.c
@@ -2,13 +2,27 @@
 
 void	radix_sort(t_node **a, t_node **b, int size)
 {
-	t_node	*temp;
+	int		tbits;
+	int		bit;
 	int		i;
 
-	i = 0;
-	normalize(*a, size);
-	while (i < size)
+	tbits = 0;
+	bit = 0;
+	while ((1 << tbits <= size))
+		tbits++;
+	while (bit < tbits)
 	{
-		
+		i = 0;
+		while (i < size)
+		{
+			if (((*a)->value >> bit) & 1)
+				ra(a);
+			else
+				pb(a, b);
+			i++;
+		}
+		while (*b)
+			pa(a, b);
+		bit++;
 	}
 }
