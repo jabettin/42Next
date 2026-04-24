d198b1268968bac2fe78aa420ae8d45e6835dab9
Jacob Betting <jacobahbetting@gmail.com>
2026-04-24 15:04:24 +0200

    first POOP experience, just testing things out

---
+++ Python_Module01/ft_garden_intro.py
@@ -0,0 +1,15 @@
+class plant(self, name, height, age):
+	self.name(print"{name}")
+	self.height(print"{height}")
+	self.age(print"{age}")
+
+
+def ft_garden_intro():
+
+
+
+
+
+if __name__ == "__main__":
+	ft_garden_intro()
+
