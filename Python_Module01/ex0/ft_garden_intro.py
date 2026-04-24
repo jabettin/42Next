44a019bd7b4a958d1ac82bfa71552d690c5ce5ed
Jacob Betting <jacobahbetting@gmail.com>
2026-04-24 21:44:11 +0200

    ex0 done

---
+++ Python_Module01/ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+	name = "Rose"
+	height = "20cm"
+	age = 10
+	print("=== Welcome to my garden ===")
+	print(f"Plant: {name}")
+	print(f"Height: {height}")
+	print(f"Age: {age} days")
+	print("=== End of program ===")
