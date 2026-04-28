12c9da78556b1ab84a460910cd90c96edd3968f5
Jacob Betting <jacobahbetting@gmail.com>
2026-04-28 23:57:16 +0200

    ex02 complete

--- Python_Module01/ex02/ft_plant_growth.py
+++ Python_Module01/ex02/ft_plant_growth.py
@@ -1,27 +1,25 @@
-#!/usr/bin/env python3
 class Plant:
 	def show(self)->None:
-		print(f"{self.name}: {self.height:.1f}cm, {self.days_old} days old")
-	def grow(self)->None:
-		self.height: float = self.height + 0.8
+		print(f"{self.name}: {round(self.height,1)}cm, {self.days_old} days old")
 	def age(self)->None:
-		self.days_old: int = self.days_old + 1
+		self.days_old += 1
+	def grow(self)->None:
+		self.height += 0.8
 
 if __name__ == '__main__':
-	height: float = 10
-	age: int = 5
-	rose: Plant = None
+	initial_height: float = 25.0
+	rose: Plant | None = None
 	rose = Plant()
 	rose.name: str = "Rose"
-	rose.height: float = height
-	rose.days_old: int = age
+	rose.height: float = initial_height
+	rose.days_old: int = 30
 	print("=== Garden Plant Growth ===")
 	rose.show()
 	days: int = 1
 	while days <= 7:
 		rose.grow()
 		rose.age()
-		print(f"=== Day: {days} ===")
+		print(f"=== Day {days} ===")
 		rose.show()
 		days += 1
-	print(f"Growth this week: {round(rose.height - height, 1)}cm")
+	print(f"Growth this week: {round(rose.height - initial_height, 1)}cm")
