7027e96c82ad59e3e79b416b7a32c0a8f5228b92
Jacob Betting <jacobahbetting@gmail.com>
2026-04-30 17:50:34 +0200

    encapsulation introduction

--- Python_Module01/ex4/ft_garden_security.py
+++ Python_Module01/ex4/ft_garden_security.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 class Plant:
+    name: str
+    age: int
+    height: float
     def __init__(self, name: str, height: float, age: int) -> None:
         self.name = name
-        self.height = height
-        self.age = age
+        self.__height = height
+        self.__age = age
     def show(self):
-        print(f"Plant created: {self.name}: {self.height}cm, {self.age} days old")
-
-    def get_height(self):
+        print(f"Plant created: {self.name}: {get_height(self.__height)}cm, {get_age(self.__age)} days old")
+    def set_age(self, self.__age) -> int:
+        self.__new_age = input()
+        self.__age = self.__new_age
+    def set_height(self, self.__height) -> float:
+        self.__new_height = input()
+        self.__height = self.__new_height
+    def get_height(self) -> None:
         
-    def get_age(self):
+    def get_age(self) -> None:
 
 
 
