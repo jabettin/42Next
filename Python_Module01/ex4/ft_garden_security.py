bfe9c9a48ca7a251ae655216d75fe49d66e9d242
Jacob Betting <jacobahbetting@gmail.com>
2026-04-30 18:22:23 +0200

    wondering how to apply functions that compares an int, to instances whose data is stored as keys or values in a dict

--- Python_Module01/ex4/ft_garden_security.py
+++ Python_Module01/ex4/ft_garden_security.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 class Plant:
     name: str
-    age: int
     height: float
+    age: int
     def __init__(self, name: str, height: float, age: int) -> None:
         self.name = name
         self.__height = height
         self.__age = age
     def show(self):
-        print(f"Plant created: {self.name}: {get_height(self.__height)}cm, {get_age(self.__age)} days old")
-    def set_age(self, self.__age) -> int:
+        print(f"Plant created: {self.name}: {self.__height}cm, {self.__age} days old")
+    def set_age(self) -> int:
         self.__new_age = input()
-        self.__age = self.__new_age
-    def set_height(self, self.__height) -> float:
+        if self.__new_age > 0:
+            self.__age = self.__new_age
+        else:
+            print("Age has to be a positive int")
+    def set_height(self, __height) -> float:
         self.__new_height = input()
         self.__height = self.__new_height
-    def get_height(self) -> None:
-        
-    def get_age(self) -> None:
+
 
 
 
 if __name__ == '__main__':
     print("=== Garden Security System ===")
+    rose = Plant("rose", 25.5, 20)
+    rose.show()
+    rose.set_age()
+    rose.show()
