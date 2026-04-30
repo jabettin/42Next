cec3b99bd37d7fc08ade93e8acfa438f5aa069b0
Jacob Betting <jacobahbetting@gmail.com>
2026-04-30 15:38:38 +0200

    ex4 added

---
+++ Python_Module01/ex4/ft_garden_security.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name = name
+        self.height = height
+        self.age = age
+    def show(self):
+        print(f"Plant created: {self.name}: {self.height}cm, {self.age} days old")
+
+    def get_height(self):
+        
+    def get_age(self):
+
+
+
+if __name__ == '__main__':
+    print("=== Garden Security System ===")
