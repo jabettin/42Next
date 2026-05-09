8b8674560c9689ec1978b8926ebfaeb4f121c6e6
Jacob Betting <jacobahbetting@gmail.com>
2026-05-09 15:09:52 +0200

    added name encapsulation

--- Python_Module01/ex4/ft_garden_security.py
+++ Python_Module01/ex4/ft_garden_security.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 class Plant:
-    name: str
+    _name: str
     _height: float
     _age: int
 
     def __init__(self, name: str, height: float, age: int) -> None:
-        self.name = name
+        self._name = name
         self._height = height
         self._age = age
 
     def show(self) -> None:
-        print(f"Plant created: {self.name}: {self._height}cm, {self._age} days old")
+        print(f"Plant created: {self._name}: {self._height}cm, {self._age} days old")
 
     def get_height(self) -> float:
         return self._height
@@ -18,9 +18,12 @@ class Plant:
     def get_age(self) -> int:
         return self._age
 
+    def get_name(self) -> str:
+        return self._name
+
     def set_height(self, height: float) -> None:
         if height < 0:
-            print(f"{self.name}: Error, height can't be negative")
+            print(f"{self._name}: Error, height can't be negative")
             print("Height update rejected")
         else:
             self._height = float(height)
@@ -28,7 +31,7 @@ class Plant:
 
     def set_age(self, age: int) -> None:
         if age < 0:
-            print(f"{self.name}: Error, age can't be negative")
+            print(f"{self._name}: Error, age can't be negative")
             print("Age update rejected")
         else:
             self._age = age
@@ -39,8 +42,11 @@ if __name__ == '__main__':
     print("=== Garden Security System ===")
     rose = Plant("Rose", 15.0, 10)
     rose.show()
+    print()
     rose.set_height(25)
-    rose.set_age(30)
+    rose.set_age(20)
+    print()
     rose.set_height(-5)
     rose.set_age(-1)
-    print(f"Current state: {rose.name}: {rose.get_height()}cm, {rose.get_age()} days old")
+    print()
+    print(f"Current state: {rose.get_name()}: {rose.get_height()}cm, {rose.get_age()} days old")
