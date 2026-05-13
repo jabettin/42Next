1a2c0f083cf00346b0fe5526a7529ed97cd1d3dd
Jacob Betting <jacobahbetting@gmail.com>
2026-05-13 18:07:14 +0200

    try and except intro

---
+++ Python_Modules/Python_Module02/ex0/ft_first_exception.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+def input_temperature(tempstr: str) -> int:
+    return int(tempstr)
+
+def test_temperature() -> None:
+    print("=== Garden Temperature ===")
+    print()
+    try:
+        print("Input data is '25'")
+        temp = input_temperature("25")
+        print(f"Temperature is now {temp}°C")
+        print()
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+        print()
+
+    try:
+        print("Input data is 'abc'")
+        temp = input_temperature("abc")
+        print(f"Temperature is now {temp}°C")
+        print()
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}")
+        print()
+
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == '__main__':
+    test_temperature()
