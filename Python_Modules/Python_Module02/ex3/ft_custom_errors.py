f20cac927c5abbc37d5d005a450666a4e8232291
Jacob Betting <jacobahbetting@gmail.com>
2026-05-14 21:14:00 +0200

    corrected per flake8 and mypy norme

--- Python_Modules/Python_Module02/ex3/ft_custom_errors.py
+++ Python_Modules/Python_Module02/ex3/ft_custom_errors.py
@@ -8,13 +8,16 @@ class GardenError(Exception):
     def __str__(self) -> str:
         return f"{self.message}"
 
+
 class PlantError(GardenError):
     def __init__(self, message: str = "Unknown plant error") -> None:
         super().__init__(message)
 
+
 class WaterError(GardenError):
     def __init__(self, message: str = "Unknown water error") -> None:
         super().__init__(message)
+
 
 def test_plant_error() -> None:
     print("Testing PlantError...")
@@ -23,12 +26,14 @@ def test_plant_error() -> None:
     except PlantError as e:
         print(f"Caught PlantError as: {e}")
 
+
 def test_water_error() -> None:
     print("Testing WaterError...")
     try:
         raise WaterError("Not Enough water in the tank!")
     except WaterError as e:
         print(f"Caught WaterError as {e}")
+
 
 def test_garden_error() -> None:
     print("Testing catching all the garden errors...")
@@ -41,7 +46,7 @@ def test_garden_error() -> None:
 
 
 if __name__ == '__main__':
-    print ("=== Custom garden Errors Demo ===")
+    print("=== Custom garden Errors Demo ===")
     test_plant_error()
     print()
     test_water_error()
