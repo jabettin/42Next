9171e1eb9249e45a832c748837d996a1305d3806
Jacob Betting <jacobahbetting@gmail.com>
2026-05-15 15:07:35 +0200

    added basic ex4

--- Python_Modules/Python_Module02/ex4/ft_finally_block.py
+++ Python_Modules/Python_Module02/ex4/ft_finally_block.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
+class GardenError(Exception):
+    def __init__(self, message: str = "Unknown Garden Error"):
+        super().__init__(message)
+        self.message = message
 
+    def __str__(self) -> str:
+        return f"{self.message}"
+
+
+class PlantError(GardenError):
+    def __init__(self, message: str = "Unknown Plant Error"):
+        super().__init__(message)
+
+
+def water_plant(plant_name: str):
+    if islower(plant_name):
+        raise PlantError(f"Caught PlantError: Invalid plant name to water: {plant_name}")
+
+def test_watering_system() -> None:
+    print("Opening watering system")
+    for plants in [water_plant("Tomato"), water_plant("Lettuce"), water_plant("Carrots")]:
+        try:
