47ab59a2e429d275094e308199e3865d0f387662
Jacob Betting <jacobahbetting@gmail.com>
2026-05-17 13:27:29 +0200

    ex1's logic done and general use finished

---
+++ Python_Modules/Python_Module03/ex1/ft_score_analytics.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+if __name__ == '__main__':
+    print('=== Player Score Analytics ===')
+    argc = len(sys.argv)
+    scores = []
+    if argc == 1:
+        print("No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+    else:
+        for arg in sys.argv[1: ]:
+            try:
+                scores.append(int(arg))
+            except ValueError:
+                print(f"Invalid parameter: '{arg}'")
+        if len(scores) == 0:
+            print(f"No scores provided. Usage: python3 ft_score_analytics.py <score1> <score2> ...")
+        else:
+            print(f"Scores processed: {scores}")
+            print(f"Total players: {len(scores)}")
+            print(f"Average score: {round(sum(scores) / len(scores), 1)}")
+            print(f"High score: {max(scores)}")
+            print(f"Low score: {min(scores)}")
+            print(f"Score range: {max(scores) - min(scores)}")
