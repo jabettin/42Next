c86d34957109326c624dc0b1141f3c8d6fa7420d
Jacob Betting <jacobahbetting@gmail.com>
2026-05-18 22:55:02 +0200

    conversion -set(all_achievements)

--- Python_Modules/Python_Module03/ex3/ft_achievement_tracker.py
+++ Python_Modules/Python_Module03/ex3/ft_achievement_tracker.py
@@ -14,6 +14,7 @@ def gen_player_achievements() -> set[str]:
     count = random.randint(5, 9)
     return set(random.sample(ALL_ACHIEVEMENTS, count))
 
+
 def main() -> None:
     print('=== Achievement Tracker System ===')
     players: dict[str, set[str]] = {
@@ -22,8 +23,16 @@ def main() -> None:
         'Charlie': gen_player_achievements(),
         'Dylan': gen_player_achievements(),
     }
+
     for name, achievements in players.items():
         print(f"Player {name}: {achievements}")
+
+    all_achievements: set[str] = set(ALL_ACHIEVEMENTS)
+    player_sets = list(players.values())
+
+    all_unlocked: set[str] = set()
+    for achievements in player_sets:
+        all_unlocked = all_unlocked.union(achievements)
 
 if __name__ == '__main__':
     main()
