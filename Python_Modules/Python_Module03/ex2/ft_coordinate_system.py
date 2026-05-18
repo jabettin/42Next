f075d07925a91caf2046218acef3681efcbfa789
Jacob Betting <jacobahbetting@gmail.com>
2026-05-18 18:37:05 +0200

    Done with ex2

--- Python_Modules/Python_Module03/ex2/ft_coordinate_system.py
+++ Python_Modules/Python_Module03/ex2/ft_coordinate_system.py
@@ -16,11 +16,16 @@ def get_player_pos() -> tuple:
                 print(f"Error on parameter '{part}': {e}")
         else:
             print("Invalid syntax")
-            continue
 
 if __name__ == '__main__':
     print('=== Game Coordinate System ===')
     print()
     print('Get a first set of coordinates')
-    result = get_player_pos()
-    print(result)
+    x1, y1, z1 = get_player_pos()
+    print(f"Got a first tuple: {(x1, y1, z1)}")
+    print(f"It includes: X={x1}, Y={y1}, Z={z1}")
+    print(f"Distance to center: {round(math.sqrt(x1**2 + y1**2 + z1**2), 4)}")
+    print()
+    print('Get a second set of coordinates')
+    x2, y2, z2 = get_player_pos()
+    print(f"Distance between the 2 sets of coordinates: {round(math.sqrt((x2-x1)**2 + (y2-y1)**2 + (z2-z1)**2), 4)}")
