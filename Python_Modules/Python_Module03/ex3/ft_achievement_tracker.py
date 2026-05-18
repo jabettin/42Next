8bc2dd2b14cc52c9365b782cb230897b859ef184
Jacob Betting <jacobahbetting@gmail.com>
2026-05-18 19:03:30 +0200

    Made list and finished gen_player_achievements

--- Python_Modules/Python_Module03/ex3/ft_achievement_tracker.py
+++ Python_Modules/Python_Module03/ex3/ft_achievement_tracker.py
@@ -2,4 +2,18 @@
 
 import random
 
-def gen_player_achievements()
+ALL_ACHIEVEMENTS = [
+    "First Steps", "Speed Runner", "Untouchable", "Survivor",
+    "Boss Slayer", "World Savior", "Master Explorer", "Treasure Hunter",
+    "Collector Supreme", "Crafting Genius", "Strategist", "Unstoppable",
+    "Sharp Mind", "Hidden Path Finder", "Dragon Slayer", "Night Owl",
+    "Team Player",
+]
+
+def gen_player_achievements() -> set[str]:
+    count = random.randint(5, 9)
+    return set(random.sample(ALL_ACHIEVEMENTS, count))
+
+def main() -> None:
+    print('=== Achievement Tracker System ===')
+    Alice
