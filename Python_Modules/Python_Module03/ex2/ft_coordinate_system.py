7a4e41113ba04546181500cf9193e494159d983a
Jacob Betting <jacobahbetting@gmail.com>
2026-05-18 17:24:20 +0200

    added error exception handling

--- Python_Modules/Python_Module03/ex2/ft_coordinate_system.py
+++ Python_Modules/Python_Module03/ex2/ft_coordinate_system.py
@@ -2,13 +2,25 @@
 
 import math
 
-def get_player_pos() -> None:
-    try:
-        for x
-
+def get_player_pos() -> tuple:
+    while True:
+        coords = input("Enter new coordinates as floats in format 'x,y,z': ")
+        parts = coords.split(',')
+        if len(parts) == 3:
+            try:
+                converted = []
+                for part in parts:
+                    converted.append(float(part))
+                return (converted[0], converted[1], converted[2])
+            except ValueError as e:
+                print(f"Error on parameter '{part}': {e}")
+        else:
+            print("Invalid syntax")
+            continue
 
 if __name__ == '__main__':
     print('=== Game Coordinate System ===')
     print()
     print('Get a first set of coordinates')
-    get_player_pos()
+    result = get_player_pos()
+    print(result)
