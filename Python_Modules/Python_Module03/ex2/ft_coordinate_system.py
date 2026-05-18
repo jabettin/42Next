b8cefb8cf4e210325c9444a64c6b4422efd13b02
Jacob Betting <jacobahbetting@gmail.com>
2026-05-18 16:30:18 +0200

    ex2 added

---
+++ Python_Modules/Python_Module03/ex2/ft_coordinate_system.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import math
+
+def get_player_pos() -> None:
+    try:
+        for x
+
+
+if __name__ == '__main__':
+    print('=== Game Coordinate System ===')
+    print()
+    print('Get a first set of coordinates')
+    get_player_pos()
