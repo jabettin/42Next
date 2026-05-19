1773e2e1bbabbb469f8615e3d637ff0d72f41c64
Jacob Betting <jacobahbetting@gmail.com>
2026-05-19 15:57:43 +0200

    added two lists

--- Python_Modules/Python_Module03/ex5/ft_data_stream.py
+++ Python_Modules/Python_Module03/ex5/ft_data_stream.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
 
 import random
+import typing
 
+PLAYERS = [
+    "bob",
+    "alice",
+    "dylan",
+    "charlie",
+]
 
+ACTIONS = [
+    "run",
+    "eat",
+    "sleep",
+    "grab",
+    "move",
+    "climb",
+    "swim",
+    "release",
+]
+
+def gen_event() -> None:
+    
 
 def main() -> None:
     pass
