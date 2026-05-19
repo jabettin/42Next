46b6d9efb585077df5569deeaa40fd8926ba6f23
Jacob Betting <jacobahbetting@gmail.com>
2026-05-19 17:24:07 +0200

    ex5 done, flake8 mypy also handled

--- Python_Modules/Python_Module03/ex5/ft_data_stream.py
+++ Python_Modules/Python_Module03/ex5/ft_data_stream.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import random
-import typing
+from typing import Generator
 
 PLAYERS = [
     "bob",
@@ -21,11 +21,34 @@ ACTIONS = [
     "release",
 ]
 
-def gen_event() -> None:
-    
+
+def gen_event() -> Generator[tuple[str, str], None, None]:
+    while True:
+        yield (random.choice(PLAYERS), random.choice(ACTIONS))
+
+
+def consume_event(
+    events: list[tuple[str, str]]
+        ) -> Generator[tuple[str, str], None, None]:
+    while len(events) > 0:
+        index = random.randint(0, len(events) - 1)
+        event = events.pop(index)
+        yield event
+
 
 def main() -> None:
-    pass
+    event_gen = gen_event()
+    for i in range(1000):
+        name, action = next(event_gen)
+        print(f"Event {i}: Player {name} did action {action}")
+    print()
+    events = [next(event_gen) for _ in range(10)]
+    print(f"Built list of 10 events: {events}")
+    print()
+
+    for event in consume_event(events):
+        print(f"Got event from list: {event}")
+        print(f"Remains in list: {events}")
 
 
 if __name__ == '__main__':
