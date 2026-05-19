6c458fcba3fdb372c6ff5fae47b529379f05bdf3
Jacob Betting <jacobahbetting@gmail.com>
2026-05-19 14:17:10 +0200

    flake8 immediate fixes

--- Python_Modules/Python_Module03/ex4/ft_inventory_system.py
+++ Python_Modules/Python_Module03/ex4/ft_inventory_system.py
@@ -2,27 +2,30 @@
 
 import sys
 
+
 def main() -> None:
     INVENTORY = {}
     print('=== Inventory System Analysis ===')
     argc = len(sys.argv)
     if argc == 1:
-        print('No arguments provided Usage: python3 ft_inventory_system <item_name>:<quantity> ...')
+        print('No arguments provided Usage: python3'
+              'ft_inventory_system <item_name>:<quantity> ...')
     else:
         for arg in sys.argv[1:]:
             parts = arg.split(':')
             if parts[0] in INVENTORY:
-                print(f"Redundant item {parts[0]} - discarding")
+                print(f"Redundant item '{parts[0]}' - discarding")
                 continue
             if len(parts) != 2:
-                print(f"Error - invalid parameter '{parts}'")
+                print(f"Error - invalid parameter '{arg}'")
                 continue
             try:
                 int(parts[1])
             except ValueError as e:
-                print(f"Quantity error for '{parts[0]}': {e}: {parts[1]}")
+                print(f"Quantity error for '{parts[0]}': {e}")
                 continue
             INVENTORY[parts[0]] = int(parts[1])
+
 
 if __name__ == '__main__':
     main()
