1e48828fa520a4bfd02adcd013ed51a3f4f2158a
Jacob Betting <jacobahbetting@gmail.com>
2026-05-21 17:28:52 +0200

    ex1 created

--- Python_Modules/Python_Module04/ex1/ft_archive_creation.py
+++ Python_Modules/Python_Module04/ex1/ft_archive_creation.py
@@ -1,1 +1,33 @@
-pass
+#!/usr/bin/env python3
+
+import sys
+import typing
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print('Usage: ft_ancient_text.py <file>')
+        return
+
+    filename: str = sys.argv[1]
+    f: typing.IO[str] | None = None
+
+    print('=== Cyber Archives Recovery ===')
+    print(f"Accessing file '{filename}'")
+    try:
+        f = open(filename)
+        print('---')
+        print(f.read(), end='')
+        print('---')
+    except OSError as e:
+        print(f"Error opening file '{filename}': {e}")
+    finally:
+        if f is not None:
+            f.close()
+            print(f"File '{filename}' closed.")
+
+    
+
+
+if __name__ == '__main__':
+    main()
