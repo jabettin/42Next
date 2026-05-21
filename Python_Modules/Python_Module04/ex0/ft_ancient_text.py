b185ecae4db84b8bb7e857413839d4dd48be9e13
Jacob Betting <jacobahbetting@gmail.com>
2026-05-21 15:37:21 +0200

    Pythonic coded and finished

--- Python_Modules/Python_Module04/ex0/ft_ancient_text.py
+++ Python_Modules/Python_Module04/ex0/ft_ancient_text.py
@@ -5,25 +5,26 @@ import typing
 
 
 def main() -> None:
-    argc = len(sys.argv)
-    if argc == 2:
-        print('=== Cyber Archives Recovery ===')
-        try:
-            print(f"Accessing file '{sys.argv[1]}'")
-            f = open(sys.argv[1])
-            filename = sys.argv[1]
-            print('---')
-            print()
-            print(f.read())
-            print()
-            print('---')
-        except OSError as e:
-            print(f"Error opening file '{filename}': {e}")
-        print(f"File {filename} closed.")
-        finally:
-            f.close(filename)
-    else:
+    if len(sys.argv) != 2:
         print('Usage: ft_ancient_text.py <file>')
+        return
+
+    filename: str = sys.argv[1]
+    f: typing.IO[str] | None = None
+
+    print('=== Cyber Archives Recovery ===')
+    print(f"Accessing file '{filename}'")
+    try:
+        f = open(filename)
+        print('---')
+        print(f.read(), end='')
+        print('---')
+    except OSError as e:
+        print(f"Error opening file '{filename}': {e}")
+    finally:
+        if f is not None:
+            f.close()
+            print(f"File '{filename}' closed.")
 
 
 if __name__ == '__main__':
