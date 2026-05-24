e003069eafee31e48ddc04b08421ebbb1a7372db
Jacob Betting <jacobahbetting@gmail.com>
2026-05-24 17:15:41 +0200

    added print statements per subject example, ex3 done"

--- Python_Modules/Python_Module04/ex3/ft_vault_security.py
+++ Python_Modules/Python_Module04/ex3/ft_vault_security.py
@@ -16,12 +16,16 @@ def secure_archive(filename: str, action: str = 'r', content: str = '') -> tuple
 
 def main() -> None:
     print('=== Cyber Archives Security ===')
+    print("Using 'secure_archive' to read from a nonexistent file:")
     result1 = secure_archive("non/existent/file")
     print(result1)
+    print("Using 'secure_archive' to read from an inaccessible file:")
     result2 = secure_archive("/etc/shadow")
     print(result2)
+    print("Using 'secure_archive' to read from a regular file:")
     result3 = secure_archive("ancient_fragment.txt")
     print(result3)
+    print("Using 'secure_archive' to write previous content to a new file:")
     result4 = secure_archive("newfile.txt", 'w', result3[1])
     print(result4)
 
