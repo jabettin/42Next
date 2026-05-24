09ac6084cf717b219a428374205cd90b2ff6c72e
Jacob Betting <jacobahbetting@gmail.com>
2026-05-24 17:00:55 +0200

    general structure for ex3 down

--- Python_Modules/Python_Module04/ex3/ft_vault_security.py
+++ Python_Modules/Python_Module04/ex3/ft_vault_security.py
@@ -1,2 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
+
+def secure_archive(filename: str, action: str = 'r', content: str = '') -> tuple[bool, str]:
+    try:
+        with open(filename, action) as f:
+            if action == 'r':
+                return (True, f.read())
+            elif action == 'w':
+                f.write(content)
+                return (True, 'Content successfully written to file')
+    except OSError as e:
+        return (False, str(e))
+
+
+def main() -> None:
+    print('=== Cyber Archives Security ===')
+    secure_archive("ancient_fragments.txt", "r",)
+    secure_archive("ancient_fragments.txt", "w", "hello")
+
+
+if __name__ == '__main__':
+    main()
