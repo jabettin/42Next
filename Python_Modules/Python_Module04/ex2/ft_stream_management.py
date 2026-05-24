f8568ea882f68bdd47a6341e0499bb825a06cecd
Jacob Betting <jacobahbetting@gmail.com>
2026-05-24 15:19:16 +0200

    added ex2, finishing module 04 before the end of this week

---
+++ Python_Modules/Python_Module04/ex2/ft_stream_management.py
@@ -0,0 +1,60 @@
+#!/usr/bin/evn python3
+
+import sys
+import typing
+
+
+def transform_data(content: str) -> str:
+    return '\n'.join(line + '#' for line in content.splitlines())
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print('Usage: ft_archive_creation.py <file>')
+        return
+
+    filename: str = sys.argv[1]
+    f: typing.IO[str] | None = None
+
+    print('=== Cyber Archives Recovery & Preservation ===')
+    print(f"Accessing file '{filename}'")
+    try:
+        f = open(filename)
+        content = f.read()
+        print('---')
+        print(content, end='')
+        print('---')
+    except OSError as e:
+        print(f"Error opening file '{filename}': {e}")
+        return
+    finally:
+        if f is not None:
+            f.close()
+            print(f"File '{filename}' closed.")
+    print()
+    transformed = transform_data(content)
+    print('Transform data:')
+    print('---')
+    print(transformed)
+    print('---')
+    new_file = input('Enter new file name (or empty): ')
+    if new_file:
+        out_file: typing.IO[str] | None = None
+        print(f"Saving data to '{new_file}'")
+        try:
+            out_file = open(new_file, 'w')
+            out_file.write(transformed)
+        except OSError as e:
+            print(f"Error opening file '{new_file}': {e}")
+        finally:
+            if out_file is not None:
+                out_file.close()
+                print(f"Data saved in file '{new_file}'.")
+            else:
+                print("Data not saved.")
+    else:
+        print('Not saving data.')
+
+
+if __name__ == '__main__':
+    main()
