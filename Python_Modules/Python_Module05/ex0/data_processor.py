d7a3362120014c7e94d8c426b1ebb28df5277a0b
Jacob Betting <jacobahbetting@gmail.com>
2026-06-02 19:01:48 +0200

    mypy && flake8 adjustments

--- Python_Modules/Python_Module05/ex0/data_processor.py
+++ Python_Modules/Python_Module05/ex0/data_processor.py
@@ -8,12 +8,15 @@ class DataProcessor(ABC):
     def __init__(self) -> None:
         self._data: list[tuple[int, str]] = []
         self._rank: int = 0
+
     @abstractmethod
     def validate(self, data: Any) -> bool:
         pass
+
     @abstractmethod
     def ingest(self, data: Any) -> None:
         pass
+
     def output(self) -> tuple[int, str]:
         return self._data.pop(0)
 
