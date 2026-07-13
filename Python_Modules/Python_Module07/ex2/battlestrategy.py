6bf24847c98d7658f4f5cd833c596ba9dc4c3a9f
Jacob Betting <jacobahbetting@gmail.com>
2026-07-13 17:21:52 +0200

    added error class with raises at act()

--- Python_Modules/Python_Module07/ex2/battlestrategy.py
+++ Python_Modules/Python_Module07/ex2/battlestrategy.py
@@ -1,26 +1,47 @@
 from abc import ABC, abstractmethod
-
+from ex0 import Creature
+from ex1 import HealCapability, TransformCapability
 
 class BattleStrategy(ABC):
     @abstractmethod
-    def is_valid(self) -> bool:
+    def is_valid(self, creature: Creature) -> bool:
         ...
 
     @abstractmethod
-    def act(self) -> None:
+    def act(self, creature: Creature) -> None:
         ...
 
 
-    class NormalStrategy(BattleStrategy):
-        def act(self) -> None:
-            creature
+class InvalidStrategyError(Exception):
+    ...
 
-    
-    class AggressiveStrategy(BattleStrategy):
-        ...
 
-    
-    class DefensiveStrategy(BattleStrategy):
-        ...
+class NormalStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        return True
 
-    
+    def act(self, creature: Creature) -> None:
+        print(creature.attack())
+
+
+class AggressiveStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        return isisnstance(creature, TransformCapability)
+
+    def act(self, creature: Creature) -> None:
+        if not self._is_valid(creature):
+            raise InvalidStrategyError(f"Invalid Creature '{creature._name}' for this aggressive strategy")
+        print(creaure.transform())
+        print(creature.attack())
+        print(creature.revert())
+
+
+class DefensiveStrategy(BattleStrategy):
+    def is_valid(self, creature: Creature) -> bool:
+        return isisnstance(creature, HealCapability)
+
+    def act(self, creature: Creature) -> None:
+        if not self._is_valid(creature):
+            raise InvalidStrategyError(f"Invalid Creature '{creature._name}' for this defensive strategy")
+        print(creature.attack())
+        print(creature.heal())
--- Python_Modules/Python_Module07/ex2/creatures.py
+++ Python_Modules/Python_Module07/ex2/creatures.py
@@ -1,2 +1,21 @@
 from ex0 import Creature
 from ex1 import HealCapability, TransformCapability
+
+class Shiftling(Creature, TransformCapability):
+    def __init__(self) -> None:
+        Creature.__init__(self, "Shiftling", "Normal")
+        TransformCapability.__init__(self)
+
+    def transform(self) -> str:
+        self._transformed = True
+        return f"{self._name} shifts into a sharper form!"
+
+    def revert(self) -> str:
+        self._transformed = False
+        return f"{self._name} returns to normal"
+
+    def attack(self) -> str:
+        if self._transformed:
+            return f"{self._name} performs a boosted strike!"
+        else:
+            return f"{self._name} attacks normally."
