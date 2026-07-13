1ca7543ecd25f92a97093811792c55e91c1ef449
Jacob Betting <jacobahbetting@gmail.com>
2026-07-13 21:12:49 +0200

    deleted old creatures file

--- Python_Modules/Python_Module07/tournament.py
+++ Python_Modules/Python_Module07/tournament.py
@@ -4,4 +4,6 @@ from ex2 import BattleStrategy, NormalStrategy, AggressiveStrategy, DefensiveStr
 
 
 def battle(opponents: list[tuple[CreatureFactory, BattleStrategy]]) -> None:
-    
+    for i, (factory_a, factory_b) in enumerate(opponents)
+    try:
+
