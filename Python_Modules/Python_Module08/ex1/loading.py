1462d75015a0f0d88ac05b07f624dedc2c06a6b6
Jacob Betting <jacobahbetting@gmail.com>
2026-07-23 19:26:59 +0200

    start in fallback data

--- Python_Modules/Python_Module08/ex1/loading.py
+++ Python_Modules/Python_Module08/ex1/loading.py
@@ -30,8 +30,27 @@ def api_call(modules: dict) -> None:
         r = requests_module.get("https://api.worldbank.org/v2/country/all/indicator/SP.POP.TOTL?format=json&per_page=300&date=2022")
         r.raise_for_status()
         data = r.json()
-        print(type(data), len(data))
-        print(data[0])
-        print(data[1][:2])
     except requests_module.exceptions.RequestException as e:
         print(f"Your API request has failed: {e}")
+        return
+    return data[1]
+
+def fallback_data_gen() -> list:
+    country_names = [
+        "The Netherlands",
+        "Germany",
+        "Belgium",
+        "UK",
+        "France",
+        "Switzerland",
+        "Poland"
+    ]
+    countries = {}
+    generated_pop = numpy.random.randint(10000000000, size=(125, 1))
+    for names, pop in enumerate(country_names):
+        countries[country_names] = generated_pop
+        print(names, pop)
+
+
+def data_validator() -> None:
+    
